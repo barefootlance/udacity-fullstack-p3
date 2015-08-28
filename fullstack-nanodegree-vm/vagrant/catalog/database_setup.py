819dbed53a745d24f0740f24ea5e53eab395daf0
Barefoot Lance <barefootlance@gmail.com>
2015-08-27 22:32:39 -0700

    use postresql database

--- fullstack-nanodegree-vm/vagrant/catalog/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/catalog/database_setup.py
@@ -141,5 +141,6 @@ class Item(Base):
        }
 
 
-engine = create_engine('sqlite:///catalog.db')
+#engine = create_engine('sqlite:///catalog.db')
+engine = create_engine("postgresql+psycopg2://catalog:catalog@/catalog")
 Base.metadata.create_all(engine)
